cdebb320e52503c0092e69fe21f4c391fee05293
Heidi Hlavinka <hchlav@gmail.com>
2018-03-30 01:13:05 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -20,7 +20,7 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-  shipping_manifest["oil paintings"]# Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -29,7 +29,7 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
+  shipping_manifest["muskets" => 2]# add 2 muskets to the shipping_manifest hash below
 
 
 
